78461fd06766a9ae3d99f095219493015e141896
wjahatsyed <wajahatsyed1257@gmail.com>
2026-06-29 18:34:31 +0500

    fix: minor fixes

--- common/src/test/java/com/wajahat/ordersaga/common/enums/EnumTest.java
+++ common/src/test/java/com/wajahat/ordersaga/common/enums/EnumTest.java
@@ -1,7 +1,7 @@
 package com.wajahat.ordersaga.common.enums;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 import org.junit.jupiter.api.Test;
 
